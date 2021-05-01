a5fb328f828f41b7fda3f1c3d763f702be0c9b70
deathgun3211 <romamur65@gmail.com>
2021-05-01 13:20:51 +0300

    'push_epta'

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
+              card's content.</p>
+              <h1></h1>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
